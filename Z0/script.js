75de4ebf58e480c59c8c6b1b28259bc9113405f6
Katarzyna Duchowska <k.duchowska@o2.pl>
2020-12-17 20:52:23 +0100

    Z0

---
+++ Z0/script.js
@@ -0,0 +1,27 @@
+function task1() {
+    //todo
+}
+
+function task2() {
+    //todo
+}
+
+function task3() {
+    //todo
+}
+
+function task4() {
+    //todo
+}
+
+function task5() {
+    //todo
+}
+
+function task6() {
+    //todo
+}
+
+function task7() {
+    //todo
+}
